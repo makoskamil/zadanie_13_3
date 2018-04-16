e42c060f36183311d27c5b35ecce985e49050038
Michael Gacek <gacek.michael@outlook.com>
2018-04-16 22:42:11 +0200

    Update program.js

--- program.js
+++ program.js
@@ -1,13 +1,23 @@
 process.stdin.setEncoding('utf-8');
+
 process.stdin.on('readable', function() {
 	var input = process.stdin.read();
-	if(input !== null) {
+
+	if (input !== null) {
 		var instruction = input.toString().trim();
-		if (instruction === '/exit') {
-			process.stdout.write('Quitting app!\n');
-			process.exit();
-		} else {
-		process.stdout.write('Wrong instruction!\n');
+
+		switch (instruction) {
+			case '/exit':
+				process.stdout.write('Quitting app!\n');
+				process.exit();
+			case '/lang':
+				process.stdout.write(process.env.LANG + '\n');
+				break;
+			case '/version':
+				process.stdout.write(process.version + '\n');
+				break;
+			default: 
+				process.stdout.write('Wrong instruction!\n');
 		}
 	}
 });
